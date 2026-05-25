b56b5243a2c098feb8afba86123abafbc084812f
affonso25 <afonso.carrapico10@gmail.com>
2026-05-25 11:31:30 +0100

    fix: remove key pair

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,7 +74,6 @@ resource "aws_instance" "web" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.web_sg.id]
-  key_name               = var.key_name
 
   user_data = <<-EOF
  #!/bin/bash
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,12 +10,6 @@ variable "instance_type" {
   default     = "t3.micro"
 }
 
-variable "key_name" {
-  description = "EC2 Key Pair name"
-  type        = string
-  default     = "test-key"
-}
-
 variable "db_password" {
   type    = string
   default = "Password123!"
